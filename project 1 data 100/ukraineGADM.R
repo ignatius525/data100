0703c4117de8341dea5afde9faed125ee2534a3e
ignatius525 <iamiagkov@email.wm.edu>
2020-02-10 09:31:45 -0500

    ukrain

--- project 1 data 100/ukraineGADM.R
+++ project 1 data 100/ukraineGADM.R
@@ -11,22 +11,27 @@ ukr_adm2 <- read_sf("data/gadm36_UKR_shp/gadm36_UKR_2.shp")
 
 ggplot() +
   geom_sf(data = ukr_adm2,
-          size = .30,
+          size = .2,
           color = "blue2",
-          fill = "yellow",
+          fill = "yellow2",
           alpha = .65) +
   geom_sf(data = ukr_adm1,
-          size = .65,
+          size = .5,
           color = "gray50",
           alpha = .65) +
   geom_sf(data = ukr_int,
-          size = 2,
+          size = 1,
           color = "blue",
           alpha = 0) +
   geom_sf_text(data = ukr_adm2,
                aes(label = NAME_2),
-               size = .50) +
+               size = .75,
+               color = "blue3") +
   geom_sf_text(data = ukr_adm1,
                aes(label = NAME_1),
-               size = 1)
-ggsave("ukraine_adm2.png")
+               size = 2.5) +
+  geom_sf_text(data = ukr_int,
+               aes(label = NAME_0),
+               size = 4,
+               color = "blue")
+ggsave("ukraine_adm2.png", height = 23.92, width = 14.10)
